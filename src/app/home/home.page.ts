07cd8075ce732f56a94802a2810ed1064a4923ec
Srilema <90621525+Srilema@users.noreply.github.com>
2024-11-26 09:53:54 +0100

    First commit - icon added - color added to var

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { restaurantOutline, boatOutline, pizzaOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({restaurantOutline, boatOutline, pizzaOutline});
+  }
 }
