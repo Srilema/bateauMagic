411802940a48577a0b58569b46446949d9355fbc
Sofia <Sofia@BENARAIS>
2024-11-27 14:23:01 +0100

    merge + vue description

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,12 +11,12 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'description',
-    loadComponent: () => import('./description/description.page').then( m => m.DescriptionPage)
+    path: 'item-list/:categorie',
+    loadComponent: () => import('./item-list/item-list.page').then( m => m.ItemListPage)
   },
   {
-    path: 'item-description',
-    loadComponent: () => import('./item-description/item-description.page').then( m => m.ItemDescriptionPage)
+    path: 'description',
+    loadComponent: () => import('./description/description.page').then( m => m.DescriptionPage)
   },
 
 ];
--- src/app/description/description.page.ts
+++ src/app/description/description.page.ts
@@ -4,36 +4,31 @@ import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonIcon, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
 import { Item } from '../models/item';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-description',
   templateUrl: './description.page.html',
   styleUrls: ['./description.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton]
+  imports: [HeaderComponent, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton]
 })
 export class DescriptionPage implements OnInit {
   Title: string = "Restaurant de l'hotel";
   Description: string = "Description du restaurant";
   photoUrl: string = "..\assets\photos\restaurant\photo.jpeg";
   isProduit: boolean = false;
+  item!: Item;
   constructor(private route : ActivatedRoute, private router: Router) { 
 
   }
-
   ngOnInit() {
-    const navigation = this.router.getCurrentNavigation();
-    const state = navigation?.extras.state as { item: any };
-
-    if (state?.item) {
-      this.Title = state.item.title;
-      this.Description = state.item.description;
-      this.photoUrl = state.item.photo;
-    }
-  }
-
-  onLoadPanier(){
-
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation()?.extras.state){
+        this.item = this.router.getCurrentNavigation()?.extras.state?.['item'];
+      }
+    })
+    
   }
     
 }
